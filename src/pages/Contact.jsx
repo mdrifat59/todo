7415a0b279f04e7a679ff4cde47d0573450fc8f3
mdrifat59 <mdrifatulislam59@gmail.com>
2024-07-12 22:54:03 +0600

    react-helmet add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 const Contact = () => {
   return (
+    <>
+    <Helmet>
+        <title>Contact</title>
+    </Helmet>
     <div>Contact</div>
+    </>
   )
 }
 
--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 const Task = () => {
   return (
+    <>
+    <Helmet>
+        <title>Task-view</title>
+    </Helmet>
     <div>Task</div>
+    </>
   )
 }
 
