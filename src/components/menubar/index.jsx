18e76c477e667baea5170c787b913d37e79f8d60
mdrifat59 <mdrifatulislam59@gmail.com>
2024-07-15 12:13:59 +0600

    Task-views page design complete

--- src/components/menubar/index.jsx
+++ src/components/menubar/index.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
             <div className='bg-sky-400 flex justify-between mt-5'> 
                 <div className='flex justify-start p-4'> <h3>Logo</h3></div>
                 <div className='flex gap-4 items-center mr-20'>  
-                  <Link to="/" className='border-2 border-blue-500 text-white hover:text-black  hover:bg-white py-1 px-4 rounded-md ' href="#">Home</Link>
-                  <Link to ="/task-view" className='border-2 border-blue-500 text-white hover:text-black  hover:bg-white py-1 px-4 rounded-md ' href="#">Task View</Link>
-                  <Link to="/contact" className='border-2 border-blue-500 text-white hover:text-black  hover:bg-white py-1 px-4 rounded-md ' href="#">Contact</Link>
+                  <Link to="/" className='border-2 border-blue-500 text-white hover:text-black  hover:bg-white py-1 px-4 rounded-md '>Home</Link>
+                  <Link to ="/task-view" className='border-2 border-blue-500 text-white hover:text-black  hover:bg-white py-1 px-4 rounded-md '>Task View</Link>
+                  <Link to="/contact" className='border-2 border-blue-500 text-white hover:text-black  hover:bg-white py-1 px-4 rounded-md '>Contact</Link>
                 </div> 
             </div>
         </div>
--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
-import { Helmet } from 'react-helmet-async'
+import { Helmet } from 'react-helmet-async' 
+import { useSelector } from 'react-redux'
+import { formatDistance,} from 'date-fns'
 
-const Task = () => {
+const Task = () => {  
+  let taskView = useSelector((state)=>state.task.tasks)
   return (
     <>
     <Helmet>
         <title>Task-view</title>
     </Helmet>
-    <div>Task</div>
+        <div className='container'>
+          <h1 className='text-center text-4xl my-2'>All Task</h1>
+          <div className='grid grid-cols-4 gap-3 mt-5'>
+            {taskView.map(item=>(
+            <div className='border border-black  rounded-md shadow-xl p-3'>
+                    <h2><span className='font-bold'>Name:</span> {item.name}</h2>
+                    <h3><span className='font-bold'>Title:</span> {item.title}</h3>  
+                    <p><span className='font-bold'>Description:</span> {item.description}</p> 
+                    <h4>{formatDistance(item.createdAt, new Date(), { addSuffix: true })}</h4> 
+                    <div className='flex justify-end gap-3'>
+                      <button className='border bg-red-500 text-white py-1 px-4 rounded-md hover:border-red-500 hover:bg-white hover:text-red-500'>Delete</button>
+                      <button className='border bg-gray-500 text-white py-1 px-4 rounded-md hover:border-gray-500 hover:bg-white hover:text-gray-500'>Update</button>
+                    </div>
+            </div>
+            ))}
+            
+
+          </div>
+        </div>
     </>
   )
 }
