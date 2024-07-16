5ee526ad01ecf63a4a0d5695a55a15e18d93af28
mdrifat59 <mdrifatulislam59@gmail.com>
2024-07-16 21:20:38 +0600

    store name change

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { HelmetProvider } from 'react-helmet-async' 
-import { Provider } from 'react-redux' 
-import store from './features/store.jsx'
+import { Provider } from 'react-redux'  
+import store from './features/Store.jsx'
  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
